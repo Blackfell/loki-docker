639712318b3da33d090471616f93925df70f2623
hullet <hullet@ling.local>
2020-12-11 15:14:51 +0000

    Added root check

--- run_loki.sh
+++ run_loki.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
+if [ $EUID != 0 ] ; then echo "You must run this script as root"; exit; fi
 YOURXCOOKIE=$(xauth list) && echo "[+] - Got xcookie to give to Loki container..." || echo "[!] - Couldn't get xcookie!"
-#docker cp /tmp/YOURXCOOKIE loki:/opt/XCOOKIE && echo "[+] - Success." || echo  "[!] - Oh no, fail!"
-#rm /tmp/YOURXCOOKIE
-#echo "[+] - Cookie cleaned up on system."
-docker run -ti --net=host --privileged -v /opt/loki-data:/root -v /usr/share/wordlists:/root/wordlists -w /root -e DISPLAY=$DISPLAY -e YOURXCOOKIE="$YOURXCOOKIE" loki:0.6
-#docker start -ai loki
-#docker stop loki
-#docker rm loki
-
+docker run -ti --net=host --privileged -v /opt/loki-data:/root -w /root -e DISPLAY=$DISPLAY -e YOURXCOOKIE="$YOURXCOOKIE" loki:0.6
