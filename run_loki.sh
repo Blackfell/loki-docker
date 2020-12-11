04570b84908b6e13cedf2a538dcaad34f0104fd3
hullet <hullet@ling.local>
2020-12-11 15:44:26 +0000

    Added mkdir for loki-data in opt

--- run_loki.sh
+++ run_loki.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 if [ $EUID != 0 ] ; then echo "You must run this script as root"; exit; fi
+mkdir -p /opt/loki-data
 YOURXCOOKIE=$(xauth list) && echo "[+] - Got xcookie to give to Loki container..." || echo "[!] - Couldn't get xcookie!"
 docker run -ti --net=host --privileged -v /opt/loki-data:/root -w /root -e DISPLAY=$DISPLAY -e YOURXCOOKIE="$YOURXCOOKIE" blackfell/loki:0.6
