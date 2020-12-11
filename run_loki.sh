e8c124e6ece6271a2fb52873c8f52a4ea9792676
hullet <hullet@ling.local>
2020-12-11 15:34:28 +0000

    added reference to docker hub image

--- run_loki.sh
+++ run_loki.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ $EUID != 0 ] ; then echo "You must run this script as root"; exit; fi
 YOURXCOOKIE=$(xauth list) && echo "[+] - Got xcookie to give to Loki container..." || echo "[!] - Couldn't get xcookie!"
-docker run -ti --net=host --privileged -v /opt/loki-data:/root -w /root -e DISPLAY=$DISPLAY -e YOURXCOOKIE="$YOURXCOOKIE" loki:0.6
+docker run -ti --net=host --privileged -v /opt/loki-data:/root -w /root -e DISPLAY=$DISPLAY -e YOURXCOOKIE="$YOURXCOOKIE" blackfell/loki:latest
